a5464506fd987a7adea2f103820276a3165ebdad
al-kuresh <kureshreush@gmail.com>
2024-11-05 21:15:37 +0600

    php files

---
+++ php files/Front.php
@@ -0,0 +1,19 @@
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>
+      Sirius A Learning platform
+    </title>
+  </head>
+  <body>
+     <nav>
+      <label>BEING SIRIUS</label>
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">Admission</a></li>
+        <li><a href="">Login</a></li>
+        <li><a href="">Conatct Us</a></li>
+      </ul>
+     </nav> 
+  </body>
+</html>
