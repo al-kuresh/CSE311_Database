243ea7cd42aec9dd6eef0ac0ca706646bba20aae
al-kuresh <kureshreush@gmail.com>
2024-11-05 22:58:42 +0600

    add a nav bar, Being Sirius Label, 4 buttons Home,admission, Contact us, Custom Login Button

--- php files/Front.php
+++ php files/Front.php
@@ -1,19 +1,19 @@
+<!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-    <title>
-      Sirius A Learning platform
-    </title>
-  </head>
-  <body>
-     <nav>
-      <label>BEING SIRIUS</label>
+    <title>Sirius A Learning Platform</title>
+    <link rel="stylesheet" type="text/css" href="Front.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">  <body>
+    <nav>
+      <h1 class="heading">BEING SIRIUS</h1>
+      <p class="sub-heading">An Educational Platform</p>
       <ul>
         <li><a href="">Home</a></li>
         <li><a href="">Admission</a></li>
-        <li><a href="">Login</a></li>
-        <li><a href="">Conatct Us</a></li>
+        <li><a href="">Contact Us</a></li>
+        <li><a href="" class="custom-button">Login</a></li>
       </ul>
-     </nav> 
+    </nav> 
   </body>
 </html>
