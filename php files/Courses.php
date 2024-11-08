9bc03257c26aeda1360a88582f1b595bf2d98192
al-kuresh <kureshreush@gmail.com>
2024-11-09 01:04:41 +0600

    added a new php file named course.php

---
+++ php files/Courses.php
@@ -0,0 +1,138 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Sirius A Learning Platform</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="Css files\Front.css">
+    </head>
+  <body>
+        <div>
+            <nav>
+                <h1 class="heading">BEING SIRIUS</h1>
+                <p class="sub-heading">An Educational Platform</p>
+                <ul>
+                    <li><a href="">Home</a></li>
+                    <li><a href="">Admission</a></li>
+                    <li><a href="">Contact Us</a></li>
+                </ul>
+            </nav> 
+        </div>
+
+        <div class="container">
+            <div class="Course-Section">
+                <h2 class="text">Science Course For Class 9-10</h2>
+            </div>
+            <div class="row">
+
+                <div class="col-md-3">
+                    <img class="course-images" src="images\Biology-10.jpg">
+                    <p>
+                        BIOLOGY OF CLASS 10
+                    </p>
+                </div>
+
+                <div class="col-md-3">  
+                    <img class="course-images" src="images\chemistry-10.jpg">
+                    <p>
+                        CHEMISTRY OF CLASS 10
+                    </p>
+                </div>
+
+                <div class="col-md-3">
+                    <img class="course-images" src="images\physics-10.jpg">
+                    <p>
+                        PHYSICS OF CLASS 10
+                    </p>
+                </div>
+
+                <div class="col-md-3">
+                    <img class="course-images" src="images\H.math.jpg">
+                    <p>
+                        HIGHER MATH OF CLASS 10
+                    </p>
+                </div>
+                
+            </div>
+        </div>
+
+
+        <div class="container">
+            <div class="Course-Section">
+                <h2 class="text">Science Course For Class 11-12</h2>
+            </div>
+            <div class="row">
+
+                <div class="col-md-3">
+                    <img class="course-images" src="images\Biology-11.jpg">
+                    <p>
+                        BIOLOGY OF CLASS 11-12
+                    </p>
+                </div>
+
+                <div class="col-md-3">  
+                    <img class="course-images" src="images\chemistry-11.jpg">
+                    <p>
+                        CHEMISTRY OF CLASS 11-12
+                    </p>
+                </div>
+
+                <div class="col-md-3">
+                    <img class="course-images" src="images\physics-11.jpg">
+                    <p>
+                        PHYSICS OF CLASS 11-12
+                    </p>
+                </div>
+
+                <div class="col-md-3">
+                    <img class="course-images" src="images\H.math-11.jpg">
+                    <p>
+                        HIGHER MATH OF CLASS 11
+                    </p>
+                </div>
+                
+            </div>
+        </div>
+
+        <div class="container">
+            <div class="Course-Section">
+                <h2 class="text">Skill Based Courses</h2>
+            </div>
+            <div class="row">
+
+                <div class="col-md-3">
+                    <img class="course-images" src="images\javascript.jpg">
+                    <p>
+                        Learn JavaScript
+                    </p>
+                </div>
+
+                <div class="col-md-3">  
+                    <img class="course-images" src="images\machine.jpg">
+                    <p>
+                        Machine Learning, 
+                        build your own model of AI
+                    </p>
+                </div>
+
+                <div class="col-md-3">
+                    <img class="course-images" src="images\database.jpg">
+                    <p>
+                        Database Management System
+                    </p>
+                </div>
+
+                <div class="col-md-3">
+                    <img class="course-images" src="images\python.jpg">
+                    <p>
+                        Learn python and python libraries
+                    </p>
+                </div>
+                
+            </div>
+        </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
--- php files/Front.php
+++ php files/Front.php
@@ -5,7 +5,7 @@
     <title>Sirius A Learning Platform</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="Css files\Front.css">
-
+  </head>
   <body>
     <nav>
       <h1 class="heading">BEING SIRIUS</h1>
@@ -52,7 +52,6 @@
     </div>
 
     <div class="container">
-
       <div class="row">
 
         <div class="col-md-4">
@@ -78,11 +77,17 @@
            They make subjects like Economics, Accounting, and Management engaging and accessible, building a strong foundation for students in the business field.
           </p>
         </div>
-
-
+        
       </div>
-
     </div>
+
+    <div class="text-center">
+      <div >
+        <h2 class="text"><a href="Courses.php">Courses We Offer</h2>
+      </div>
+    </div>
+
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
   </body>
